a17e7d9c8961c135d2b7753689452c915a0997f7
‘josh <kingorijosphat@yahoo.com>
2023-05-16 19:07:03 -0400

    .format and f methods

--- print.py
+++ print.py
@@ -2,3 +2,23 @@
 
 print("This is me")
 print(("Hello, world!"))
+
+age = 25    # this is a variable 
+
+print(age)  # you can print the name of the variable
+
+# However, ypu can always format the strings to be more dynamic. using 
+# "f" or .format(name of the variable)
+
+# We can us the f and if we do so, we going to add the name of the 
+# the variable inside the placeholder{}
+
+print(f"Jane is a great girl and she now {age} years old")
+
+# Alternative 2 - you can use the .format() method to manipulated 
+# the string further. 
+
+print("Jane is a great girl and she now {} years old.".format(age))
+
+print(f"kenyan is now block because the country is just {age} years old")
+
